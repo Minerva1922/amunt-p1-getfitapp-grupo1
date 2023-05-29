3b2f88a5ffd44d6b6c68386622bee6e5047da265
Minerva1922 <minervapv83@gmail.com>
2023-05-29 11:40:28 +0200

    navbar styles

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,29 +3,79 @@
 </script>
 
 <main>
-  <header>
-  <nav>
-    <img src="\src\assets\logo getfiteasy.png" height="123" width="260"  alt="logo"/>
-    <ul>
-      <li><a href="/quienes-somos">Quiénes Somos</a></li>
-      <li><a href="/que hacemos">Qué Hacemos</a></li>
-     <li class="submenu-padre"><a href="/servicios"> Servicios</a>
-       <ul class="submenu">
-         <li><a href="/servicios">Servicios</a></li>
-         <li><a href="/ofertas">Ofertas</a></li>
-       </ul>
-     </li>
-      <li><a href="/blog">Blog</a></li>
-      <li><a href="/registrate">Registrar</a></li>
-    </ul>
-  </nav>
-  </header>
+    <header>
+        <img src="\src\assets\logo getfiteasy.png" height="60" width="130" alt="logo"/>
 
+        <nav>
+            <ul>
+                <li><a href="/quienes-somos">Quiénes Somos</a></li>
+                <li><a href="/que-hacemos">Qué Hacemos</a></li>
+                <li class="submenu-padre">
+                    <a href="/servicios">Servicios</a>
+                    <ul class="submenu">
+                        <li><a href="/servicios">Servicios</a></li>
+                        <li><a href="/ofertas">Ofertas</a></li>
+                    </ul>
+                </li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/registrate">Registrar</a></li>
+            </ul>
+        </nav>
+    </header>
 </main>
 
 <style>
-  li{
-    background: aqua;
-  }
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 5px;
+    }
+
+    img {
+        margin-right: 20px;
+    }
+
+    nav ul {
+        display: flex;
+        list-style: none;
+        font-size: 15px;
+    }
+
+    nav ul li {
+        margin-left: 20px;
+    }
+
+    nav ul li:first-child {
+        margin-left: 0;
+    }
+
+    nav ul li a {
+        text-decoration: none;
+        color: black;
+    }
+
+    .submenu {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        padding: 10px;
+        min-width: 150px;
+    }
+
+    .submenu-padre:hover .submenu {
+        display: block;
+    }
+
+    .submenu li {
+        margin-bottom: 5px;
+    }
 </style>
+
 
