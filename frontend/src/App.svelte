70ad44dbcf0be1da8665858f5e7099afeb76dbfb
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-05-29 10:37:31 +0200

    creando menu

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,12 +5,18 @@
 <main>
   <header>
   <nav>
-    <img src="assets/logo getfiteasy.png" height="123" width="260"  alt="logo"/>
+    <img src="\src\assets\logo getfiteasy.png" height="123" width="260"  alt="logo"/>
     <ul>
-      <li>Quiénes Somos</li>
-      <li>Qué Hacemos</li>
-      <li>Servicios</li>
-      <li>Registrar</li>
+      <li><a href="/quienes-somos">Quiénes Somos</a></li>
+      <li><a href="/que hacemos">Qué Hacemos</a></li>
+     <li class="submenu-padre"><a href="/servicios"> Servicios</a>
+       <ul class="submenu">
+         <li><a href="/servicios">Servicios</a></li>
+         <li><a href="/ofertas">Ofertas</a></li>
+       </ul>
+     </li>
+      <li><a href="/blog">Blog</a></li>
+      <li><a href="/registrate">Registrar</a></li>
     </ul>
   </nav>
   </header>
