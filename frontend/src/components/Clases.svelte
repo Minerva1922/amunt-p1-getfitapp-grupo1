bc281f43dc873918aba68ebf7f20374206e99c9b
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-06-02 11:55:43 +0200

    pair programming Minerva & me

--- frontend/src/components/Clases.svelte
+++ frontend/src/components/Clases.svelte
@@ -1,21 +1,30 @@
 <script>
 export let nombre;
-export let hora;
 export let instructor;
 export let sede;
 </script>
 
 <div>
     <h1>{nombre}</h1>
-    <h1>{hora}</h1>
     <p>{instructor}</p>
     <p>{sede}</p>
 </div>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+    div {
+        margin-right: 190px;
+        margin-bottom: 28px;
+    }
+    h1 {
+        text-align: justify;
+        font-size: 35px;
+        font-weight: 900;
+        color: #254e5f;
+    }
 
-   p {
-    display: flex;
-    margin-right: 300px;
-   }
+    p {
+       display: flex;
+       color: #254e5f;
+    }
 </style>
--- frontend/src/pages/QueHacemos.svelte
+++ frontend/src/pages/QueHacemos.svelte
@@ -6,89 +6,76 @@
 <div class="dia">
     <Calendario dia="LUNES"/>
     <div class="clases-diarias">
-        <Clases hora="19h-20h" nombre="BOXEO" instructor="INSTRUCTOR: AARON HUGHES" sede="SEDE: Madrid"/>
-        <Clases hora="19h-20h" nombre="PILATES" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE: Barcelona"/>
+        <Clases hora="19h-20h" nombre="BOXEO 19-20h" instructor="INSTRUCTOR: AARON HUGHES" sede="SEDE: Madrid"/>
+        <Clases hora="19h-20h" nombre="PILATES 19-20h" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE: Barcelona"/>
     </div>
 </div>
 <hr>
 <div class="dia">
     <Calendario dia="MARTES"/>
     <div class="clases-diarias">
-        <Clases hora="16h-19h" nombre="YOGA" instructor="INSTRUCTORA: ALIYAH WILLIAMS" sede="SEDE:Madrid"/>
-        <Clases hora="19h-20h" nombre="CARDIO" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE: Barcelona"/>
+        <Clases hora="16h-19h" nombre="YOGA 16-19h" instructor="INSTRUCTORA: ALIYAH WILLIAMS" sede="SEDE:Madrid"/>
+        <Clases hora="19h-20h" nombre="CARDIO 19-20h" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE: Barcelona"/>
     </div>
 </div>
 <hr>
 <div class="dia">
     <Calendario dia="MIERCOLES"/>
     <div class="clases-diarias">
-        <Clases hora="16h-19h" nombre="GAP" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE:Madrid"/>
-        <Clases hora="19h-20h" nombre="ZUMBA" instructor="INSTRUCTORA: TESHIA MILLER" sede="SEDE: Barcelona"/>
+        <Clases hora="16h-19h" nombre="GAP 16-19h" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE:Madrid"/>
+        <Clases hora="19h-20h" nombre="ZUMBA 19-20h" instructor="INSTRUCTORA: TESHIA MILLER" sede="SEDE: Barcelona"/>
     </div>
 </div>
 <hr>
 <div class="dia">
     <Calendario dia="JUEVES"/>
     <div class="clases-diarias">
-        <Clases hora="16h-19h" nombre="CARDIO" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
-        <Clases hora="19h-20h" nombre="BOXEO" instructor="INSTRUCTORA: TESHIA MILLER" sede="SEDE:Barcelona"/>
+        <Clases hora="16h-19h" nombre="CARDIO 16-19h" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
+        <Clases hora="19h-20h" nombre="BOXEO 19-20h" instructor="INSTRUCTORA: TESHIA MILLER" sede="SEDE:Barcelona"/>
     </div>
 </div>
 <hr>
 <div class="dia">
     <Calendario dia="VIERNES"/>
     <div class="clases-diarias">
-        <Clases hora="16h-19h" nombre="ZUMBA" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
-        <Clases hora="19h-20h" nombre="BOXEO" instructor="INSTRUCTORA: Minerva" sede="SEDE: Barcelona"/>
+        <Clases hora="16h-19h" nombre="ZUMBA 16-19h" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
+        <Clases hora="19h-20h" nombre="BOXEO 19-20h" instructor="INSTRUCTORA: Minerva" sede="SEDE: Barcelona"/>
     </div>
 </div>
 <hr>
 <div class="dia">
     <Calendario dia="SABADO"/>
     <div class="clases-diarias">
-        <Clases hora="16h-19h" nombre="YOGA" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
-        <Clases hora="19h-20h" nombre="BOXEO" instructor="INSTRUCTORA: TESHIA MILLER" sede="SEDE: Barcelona"/>
+        <Clases hora="16h-19h" nombre="YOGA 16-19h" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
+        <Clases hora="19h-20h" nombre="BOXEO 19-20h" instructor="INSTRUCTORA: TESHIA MILLER" sede="SEDE: Barcelona"/>
     </div>
 </div>
 <hr>
 <div class="dia">
     <Calendario dia="DOMINGO"/>
     <div class="clases-diarias">
-        <Clases hora="16h-19h" nombre="CARDIO" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
-        <Clases hora="19h-20h" nombre="BOXEO" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE: Barcelona"/>
+        <Clases hora="16h-19h" nombre="CARDIO 16-19h" instructor="INSTRUCTOR: OMAR HARRIS" sede="Madrid"/>
+        <Clases hora="19h-20h" nombre="BOXEO 19-20h" instructor="INSTRUCTORA: NICOLE WINTER" sede="SEDE: Barcelona"/>
     </div>
 </div>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
 
-
-   body{
-
-    font-family: 'Anton', sans-serif;
-   }
+    body{
+       font-family: 'Anton', sans-serif;
+    }
 
     .dia {
-        margin-top:100px;
+        margin-top:40px;
+        margin-bottom: 40px;
         display: flex;
         width: 100%;
-        height: 400px;
+        height: 200px;
         text-align: center;
-
     }
-
     .clases-diarias {
-        display: flex;
-        width: 100%;
-        height: 400px;
-        flex-direction: column;
-        justify-content: center;
-        align-items: flex-end;
-        text-align: initial;
-
-
-
+        margin-left: auto;
     }
-
 
 </style>
