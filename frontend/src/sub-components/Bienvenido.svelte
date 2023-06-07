7e4bc3fe39eb446c9c6d9e5a1309a967c7b235b5
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-06-07 11:50:21 +0200

    ëstilos de bienvenida

--- frontend/src/sub-components/Bienvenido.svelte
+++ frontend/src/sub-components/Bienvenido.svelte
@@ -1,8 +1,18 @@
-<h1>Bienvenido Ali Siam</h1>
-<button>Barcelona</button>
-<button>Madrid</button>
+<section>
+    <h1>Bienvenido Ali Siam</h1>
+    <div>
+        <button>Barcelona</button>
+        <button>Madrid</button>
+    </div>
+</section>
 
 <style>
+
+    section {
+        max-width: 800px;
+        margin: 40px auto;
+    }
+
     h1 {
         color: #254e5f;
     }
@@ -15,7 +25,8 @@
         background-color: white;
         font-size: 15px;
     }
-    button:hover{
+
+    button:hover {
         background-color: #02b2ec;
         color: white;
     }
