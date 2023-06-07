f7b774820b7dc2f20c100baf016ff1cb9c74a9a8
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-06-07 09:30:14 +0200

    bienvenido

---
+++ frontend/src/sub-component/Bienvenido.svelte
@@ -0,0 +1,22 @@
+<h1>Bienvenido Ali Siam</h1>
+<button>Barcelona</button>
+<button>Madrid</button>
+
+<style>
+    h1 {
+        color: #254e5f;
+    }
+
+    button {
+        margin-top: 50px;
+        width: 200px;
+        height: 60px;
+        border-radius: 30px;
+        background-color: white;
+        font-size: 15px;
+    }
+    button:hover{
+        background-color: #02b2ec;
+        color: white;
+    }
+</style>
