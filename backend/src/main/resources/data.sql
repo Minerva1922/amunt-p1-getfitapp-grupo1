171c6c242418c954307f8ab720733b1fc7100890
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-06-14 11:47:13 +0200

    changes surnamesa¡ and address

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO
     subscribers(id, name, second, address, data, phone, email)
 VALUES (1, 'Minerva', 'Pedret', 'Plaza 14 Madrid', 'September', 618144754 , 'minervapv83@gmail.com'),
-       (2, 'Jhoana', 'Vicente', 'Plaza Catalana 123 Barcelona', 'Enero', 618144754 , 'minervapv83@gmail.com'),
-       (3, 'Juan', 'García', 'Calle Barcelona 123 Barcelona', 'Enero', 123456789, 'juan@example.com'),
-       (4, 'María', 'López', 'Avenida Diagonal 456 Barcelona', 'Febrero', 987654321, 'maria@example.com'),
-       (5, 'Pedro', 'Martínez', 'Plaza Cataluña 789 Barcelona', 'Marzo', 654321987, 'pedro@example.com'),
-       (6, 'Ana', 'Fernández', 'Calle Gran Vía 456 Madrid', 'Enero', 111111111, 'ana@example.com'),
-       (7, 'Luis', 'Rodríguez', 'Paseo del Prado 789 Madrid', 'Febrero', 222222222, 'luis@example.com'),
-       (8, 'Laura', 'González', 'Calle Serrano 123 Madrid', 'Marzo', 333333333, 'laura@example.com'),
-       (9, 'Irina', 'Rodríguez', 'Paseo del Cañon 789 Madrid', 'Febrero', 222222222, 'irina@example.com'),
-       (10, 'Maribel', 'González', 'Calle Api 123 Madrid', 'Marzo', 333333333, 'maribel@example.com'),
-       (11, 'Joaquín', 'De La Hoz', 'Calle 23 Barcelona', 'Junio', 673408687, 'joaquin@gmail.com');
+       (2, 'Jhoana', 'Vicente', 'Plaza Catalana 123 Barcelona', 'Enero', 618144754 , 'jhoanapv83@gmail.com'),
+       (3, 'Juan', 'Medina', 'Calle Barcelona 123 Barcelona', 'Enero', 123456789, 'juan@example.com'),
+       (4, 'Eva', 'Morales', 'Avenida Diagonal 456 Barcelona', 'Febrero', 987654321, 'eva@example.com'),
+       (5, 'Pedro', 'Braco', 'Plaza Ibiza 789 Barcelona', 'Marzo', 654321987, 'pedro@example.com'),
+       (6, 'Ana', 'Pinilla', 'Calle Mare de Deu 456 Madrid', 'Enero', 111111111, 'ana@example.com'),
+       (7, 'Luis', 'Rois', 'Paseo del Prado 789 Madrid', 'Febrero', 222222222, 'luis@example.com'),
+       (8, 'Laura', 'Jarque', 'Calle Serrano 123 Madrid', 'Marzo', 333333333, 'laura@example.com'),
+       (9, 'Irina', 'Rios', 'Paseo Maragall 789 Madrid', 'Febrero', 222222222, 'irina@example.com'),
+       (10, 'Maribel', 'Birminghan', 'Calle Api 123 Madrid', 'Marzo', 333333333, 'maribel@example.com'),
+       (11, 'Joan', 'De La Hoz', 'Calle 23 Barcelona', 'Junio', 673408687, 'joaquin@gmail.com');
