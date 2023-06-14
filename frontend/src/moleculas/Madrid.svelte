c6ad199f1b4c22bcc77423f6822133ea10267816
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-06-14 14:02:42 +0200

    stylish Madrid

--- frontend/src/moleculas/Madrid.svelte
+++ frontend/src/moleculas/Madrid.svelte
@@ -4,9 +4,9 @@
 
 <section>
     {#each info as subscriptor}
-        <article>
+        <div>
             <div class="contenedor1">
-                <h3>{subscriptor.name} {subscriptor.second}</h3>
+                <h4>{subscriptor.name} {subscriptor.second}</h4>
             </div>
             <div class="contenedor2">
                 <p>{subscriptor.address}</p>
@@ -14,53 +14,28 @@
                 <p> {subscriptor.phone}</p>
                 <p> {subscriptor.email}</p>
             </div>
-        </article>
+        </div>
     {/each}
 </section>
 <hr>
 
 <style>
-    article{
-        margin-top: 10px;
-        margin-bottom: 10px;
-        display: flex;
-        width: 100%;
-        height: 200px;
-        text-align: center;
-        margin-left: 90px;
-    }
-
-    .contenedor1{
-        justify-content: flex-end;
-        position: absolute;
-
-
-    }
-
-    .contenedor2{
-        margin-left: 700px;
-
-
-
-
-    }
     section {
-        display: flex;
-        justify-content: space-around;
-        margin-top: 40px;
-        flex-direction: column;
+        background-color: red;
+
     }
 
-    h3 {
-        text-align: justify;
-        font-size: 35px;
-        font-weight: 900;
-        color: #254e5f;
+    div {
+        background-color: green;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 50px;
+        position: relative;
+    }
+    .contenedor2 {
+        display: block;
+        margin-right: 268px;
     }
 
-    p {
-        display: flex;
-        color: #254e5f;
-        margin-bottom: 5px;
-    }
 </style>
